08bfd3db43e6020fe62ded4b39399f76213a7115
LevTian <tyln2013@163.com>
2018-10-12 10:14:19 +0800

    delete input label

---
+++ 37-38/edit/js/app.js
@@ -0,0 +1,9 @@
+
+var regionWrap = document.getElementById("region-radio-wrapper");
+var productWrap = document.getElementById("product-radio-wrapper");
+
+
+createCheckBox(regionWrap, ["region", "华东", "华南", "华北"]);
+createCheckBox(productWrap, ["product", "手机", "笔记本", "智能音箱"]);
+
+
